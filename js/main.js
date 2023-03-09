66a6e82e4d7c972ce8053f4da34fe5f81d49d23c
Mihai-Cristi <apostolescu.mihaicristian@yahoo.com>
2023-03-09 17:46:21 +0200

    Finished Header, Main and Footer for Third Page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Just a starter file in case you need it
 const primaryNav = document.querySelector('.main__navigation')
 const navToggle = document.querySelector('.mobile-nav__toggle')
+const LogoWCO = document.querySelector('.logo_white_capsoff_button')
 
 navToggle.addEventListener('click', () => {
   const visibility = primaryNav.getAttribute('data-visible')
@@ -9,10 +10,14 @@ navToggle.addEventListener('click', () => {
     console.log(visibility)
     primaryNav.setAttribute('data-visible', true)
     navToggle.setAttribute('data-expanded', true)
+		LogoWCO.setAttribute('data-expanded', true)
   }
   if (visibility === "true") {
     console.log(visibility)
     primaryNav.setAttribute('data-visible', false)
     navToggle.setAttribute('data-expanded', false)
+		LogoWCO.setAttribute('data-expanded', false)
   }
 })
+
+
